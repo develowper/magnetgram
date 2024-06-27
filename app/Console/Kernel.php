d60e004a371e71ee4ab6a2c2aefbb111bc19748a
MMR <moj2raj2@gmail.com>
2024-06-27 14:49:51 +0330

    init

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,6 +7,11 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
+    protected function scheduleTimezone()
+    {
+        return 'Asia/Tehran';
+    }
+
     /**
      * Define the application's command schedule.
      *
