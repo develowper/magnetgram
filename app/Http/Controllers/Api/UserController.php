a1a44dc17ae149ea9d549f3ea65933554edc53e3
MMR <moj2raj2@gmail.com>
2024-07-02 21:14:52 +0330

    init

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -366,7 +366,7 @@ class UserController extends Controller
         if (!$user->active)
             return response()->json(['status' => 'error', 'message' => sprintf(__('*_deactivated'), __('user'))], 200);
         $user->makeVisible(['password']);
-        if (auth()->attempt($request->only('username', 'password'))) {
+        if (auth()->attempt($request->only('telegram_username', 'password'))) {
             $user->tokens()->delete();
             if ($request->push_id)
                 $user->push_id = $request->push_id;
