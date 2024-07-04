16af5fd5e9abb86171bcec2172e70bf169d5ac85
MMR <moj2raj2@gmail.com>
2024-07-05 00:30:58 +0330

    init

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,6 +42,7 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'score' => 'integer',
         'verified' => 'bool',
+        'active' => 'bool',
         'must_join' => 'array',
         'channels' => 'array',
         'groups' => 'array',
