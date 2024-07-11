81e783f59d58073ca72b56dea28d1a7df03ba0f0
MMR <moj2raj2@gmail.com>
2024-07-11 18:09:19 +0330

    init

--- routes/web.php
+++ routes/web.php
@@ -21,11 +21,11 @@ use Inertia\Inertia;
 
 Route::get('/test', function () {
 
-    foreach (DB::table('queue')->get() as $item) {
-        $u = \App\Models\User::where('telegram_id', $item->id)->first()->telegram_username ?? null;
-        if ($u)
-            DB::table('queue')->where('i', $item->i)->update(['username' => $u]);
-    }
+//    foreach (DB::table('queue')->get() as $item) {
+//        $u = \App\Models\User::where('telegram_id', $item->id)->first()->telegram_username ?? null;
+//        if ($u)
+//            DB::table('queue')->where('i', $item->i)->update(['username' => $u]);
+//    }
 
 
     foreach (\App\Models\Divar::get() as $item) {
